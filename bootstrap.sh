78583e7c0199ad02237027952d6b455e87a66b63
Alex Kostritsa <53409507+kostritsaalex@users.noreply.github.com>
2021-01-03 12:09:04 +0200

    Vagrant (w/ Ubuntu) & xDebug & PHPStorm – How to : Installation and Configuration guide
    Vagrant (w/ Ubuntu) & xDebug & PHPStorm – How to : Installation and Configuration guide
    https://academy60.com/it-operations-and-devops/vagrant-xdebug-phpstorm-how-to-installation-and-configuration-guide/

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,3 +2,6 @@
 apt update
 apt install -y php
 apt install -y mysql-server
+apt install -y php-xdebug
+cp /var/www/html/environment/xdebug.ini /etc/php/7.2/cli/conf.d/20-xdebug.ini
+systemctl restart apache2
